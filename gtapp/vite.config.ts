921a1fe0e694885c951fd54e01c542df96aaa198
Nguyễn Trường Thắng <thangntse183159@fpt.edu.vn>
2025-08-15 11:29:02 +0700

    Added support for Tailwind
    - Redesign some UI, coverting from CSS to Tailwind CSS

--- gtapp/vite.config.ts
+++ gtapp/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: "./", // Sử dụng đường dẫn tương đối khi build
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 });
